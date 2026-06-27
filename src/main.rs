bf490add877820ebed13f36d875a0c229ff8ce07
Ibrahim Mohammed <ibrahimmohammed1002@gmail.com>
2026-06-27 04:55:56 +0300

    adding actix-web basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, guard, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!\n", name)
+}
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet).guard(guard::Get()))
+            // .route("/{name}", web::get().to(greet))
+            .route("/health", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
